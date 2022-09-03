5a41030bfe890ea48fc1f580ab0747d7688b7517
ConnorDelacruz14 <Connord1432@gmail.com>
2022-09-03 15:07:22 -0700

    Display Globe Test

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-function setup() {
-    createCanvas(400, 200);
-    loadJSON(url, gotData);
-}
+// Gen random data
+const N = 300;
+const gData = [...Array(N).keys()].map(() => ({
+  lat: (Math.random() - 0.5) * 180,
+  lng: (Math.random() - 0.5) * 360,
+  size: Math.random() / 3,
+  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+}));
 
-function gotData(data) {
-    weather = data;
-}
-
-function draw() {
-    background(0);
-    if (weather) {
-        var temp = weather.main.temp;
-        var humidity = weathe.main.humidity;
-        ellipse(100, 100, temp, temp);
-    }
-}
-
-setup();
-draw();
+Globe()
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+  .pointsData(gData)
+  .pointAltitude('size')
+  .pointColor('color')
+(document.getElementById('globeViz'))
