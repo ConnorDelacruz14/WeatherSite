77ebaf81c4a005111c55b68964768ce6e91ae8a8
ConnorDelacruz14 <Connord1432@gmail.com>
2022-09-06 20:11:52 -0700

    Resizable Window

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+window.addEventListener('resize', (event) => {
+    world.width([event.target.innerWidth])
+    world.height([event.target.innerHeight])
+  });
 
-    const world = Globe()
-    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-    (document.getElementById('globeViz'));
+const world = Globe()
+.globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+.bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+(document.getElementById('globeViz'));
 
-    // custom globe material
-    const globeMaterial = world.globeMaterial();
-    globeMaterial.bumpScale = 10;
-    new THREE.TextureLoader().load('//unpkg.com/three-globe/example/img/earth-water.png', texture => {
-        globeMaterial.specularMap = texture;
-        globeMaterial.specular = new THREE.Color('grey');
-        globeMaterial.shininess = 15;
-    });
+// custom globe material
+const globeMaterial = world.globeMaterial();
+globeMaterial.bumpScale = 10;
+new THREE.TextureLoader().load('//unpkg.com/three-globe/example/img/earth-water.png', texture => {
+    globeMaterial.specularMap = texture;
+    globeMaterial.specular = new THREE.Color('grey');
+    globeMaterial.shininess = 15;
+});
 
-    setTimeout(() => { // wait for scene to be populated (asynchronously)
-        const directionalLight = world.scene().children.find(obj3d => obj3d.type === 'DirectionalLight');
-        directionalLight && directionalLight.position.set(1, 1, 1); // change light position to see the specularMap's effect
-    });
+setTimeout(() => { // wait for scene to be populated (asynchronously)
+    const directionalLight = world.scene().children.find(obj3d => obj3d.type === 'DirectionalLight');
+    directionalLight && directionalLight.position.set(1, 1, 1); // change light position to see the specularMap's effect
+});
+
